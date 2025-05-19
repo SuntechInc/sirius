0bcef95f295aeea3a68adf1a67eb6d5f4c54c3e9
lucas.previato <lucaspreviato@live.com>
2025-05-19 20:26:01 -0300

    test: teste de deploy

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,14 +4,15 @@ export default function Home() {
   return (
     <main style={{ padding: "2rem", fontFamily: "system-ui" }}>
       <h1>Bem vindo ao Quality Flow</h1>
+      <h2>Selecione uma opção:</h2>
       <nav>
         <ul>
           <li>
             {/* use o componente Link e passe o texto como filho */}
-            <Link href="/admin">Admin</Link>
+            <Link style={{color: "blue"}} href="/admin">Admin</Link>
           </li>
           <li>
-            <Link href="/dashboard">Dashboard</Link>
+            <Link style={{color: "blue"}} href="/dashboard">Dashboard</Link>
           </li>
         </ul>
       </nav>

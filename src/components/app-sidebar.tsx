b003af9617da918800ae65daa3f255fbb22f0c4a
Matheus Braúna <mattsilvahenrique102@gmail.com>
2025-07-16 23:19:18 -0300

    feat: add collapsible icon support to sidebar

--- src/components/app-sidebar.tsx
+++ src/components/app-sidebar.tsx
@@ -41,7 +41,7 @@ export function AppSidebar() {
   const pathname = usePathname()
 
   return (
-    <Sidebar>
+    <Sidebar collapsible="icon">
       <SidebarHeader>
         <SidebarMenu>
           <SidebarMenuItem>
